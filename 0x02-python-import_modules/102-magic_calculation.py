6868f6f64591fccf193daab989a8091317c61fbd
Colightech <saintchris4@gmail.com>
2023-10-05 12:58:47 -0700

    102-magic_calculation.py task

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        f = add(a, b)
+        for x in range(4, 6):
+            f = add(f, x)
+        return (f)
+
+    else:
+        return(sub(a, b))
