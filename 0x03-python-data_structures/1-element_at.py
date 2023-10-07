4e330755690a4725dbedf8ce51d4c93ff8e6451c
Colightech <saintchris4@gmail.com>
2023-10-07 08:45:32 -0700

    1-element_at.py task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     list_len = len(my_list)
-    for x in range(0, list_len):
-        if idx < 0 or idx > list_len:
-            return (0)
-        return (my_list[idx])
+    if idx < 0 or idx > list_len - 1:
+        return (0)
+    return (my_list[idx])
