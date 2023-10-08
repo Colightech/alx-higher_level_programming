62d9748f63f00bf97de3d398445b223cf0c44c45
Colightech <saintchris4@gmail.com>
2023-10-08 13:44:00 -0700

    7-add_tuple.py task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a_len = len(tuple_a)
+    if tuple_a_len < 2:
+        if tuple_a_len == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    tuple_b_len = len(tuple_b)
+    if tuple_b_len < 2:
+        if tuple_b_len == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
