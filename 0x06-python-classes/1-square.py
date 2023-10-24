2161827f5f032a09eb57ac3b4293b19808c8b834
Colightech <saintchris4@gmail.com>
2023-10-24 05:07:19 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""This defines a class sqare"""
 class Square:
+
+    """Class init method"""
     def __init__(self, size):
+
+        """initializing size"""
         self.__size = size
