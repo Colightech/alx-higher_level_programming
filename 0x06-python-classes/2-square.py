d595795f1ff27187a94808763d5a211d713cd9b0
Colightech <saintchris4@gmail.com>
2023-10-24 05:49:04 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+
+    """Instantiation with optional size"""
+    def __init__(self, size=0):
+
+        """using a try block"""
+
+        if not (size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        """initialing size"""
+        self.__size = size
