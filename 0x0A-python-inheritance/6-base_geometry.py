b9cd8da97fdb98dd90a8dcc9ae5f624f52ddc5ed
Colightech <saintchris4@gmail.com>
2023-11-06 06:15:43 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a module on BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """a method on basegeometry (base on 5-base_geometry.py)."""
+    def area(self):
+        """this method is notnimplemented"""
+        raise Exception("area() is not implemented")
