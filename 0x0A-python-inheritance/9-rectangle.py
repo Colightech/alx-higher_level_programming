9923d21347ef9e8095cd678a86bb4a1df4f8faea
Colightech <saintchris4@gmail.com>
2023-11-06 10:16:51 -0800

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a module on  class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a class that inherit the parent class BaseGeometry"""
+    def __init__(self, width, height):
+        """the init method of the class"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    
+    def area(self):
+        return self.__width *  self.__height
+
+    def __str__(self):
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
