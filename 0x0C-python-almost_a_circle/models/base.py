f7d11c64df4b40d4b3b1f2f5e23b269edbc6f599
Colightech <saintchris4@gmail.com>
2023-11-14 02:10:35 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """a module for a new class base"""
+import json
 
 
 class Base:
@@ -15,3 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """A static method that return JSON string representation
+        of a dictionary"""
+        if len(list_dictionaries) is None or len(list_dictionaries) == "":
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
