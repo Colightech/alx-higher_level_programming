ecb9674df0907a1bab0b844a0611a9b21ad003e7
Colightech <saintchris4@gmail.com>
2023-12-12 22:44:54 -0800

    101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Import in hbtn_0c_0 database this table dump
+-- Write a script that displays the average temperature
+-- (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT city, AVG(value) AS avg_tmp
+FROM temp
+GROUP BY city
+ORDER BY avg_tmp DESC;
