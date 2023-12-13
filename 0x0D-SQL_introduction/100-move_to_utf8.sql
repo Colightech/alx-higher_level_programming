b3897960b5d3fb6d46c160fe87bc76d9ca780dea
Colightech <saintchris4@gmail.com>
2023-12-12 22:35:04 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,13 +4,10 @@
 SET NAMES utf8mb4;
 SET CHARACTER SET utf8mb4;
 
--- Use the specified database
 USE hbtn_0c_0;
 
--- Convert the first_table to utf8mb4
 ALTER TABLE first_table
   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the name field in first_table to utf8mb4
 ALTER TABLE first_table
   MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
