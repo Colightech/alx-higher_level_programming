4080601f5f098f268aad9830c8e89ed3e87c294b
Colightech <saintchris4@gmail.com>
2024-01-13 01:52:23 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print("("row")")
+        print(row)
     cur.close()
     conn.close()
