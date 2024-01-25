9400c2c9a510d705a4be46b8921bffdd862c3af7
Colightech <saintchris4@gmail.com>
2024-01-25 12:52:24 -0800

    1-body.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-# Check if the URL is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-response_size=$(curl -sI "$url" | grep -i content-length | awk '{print $2}' | tr -d '\r\n')
-
-
-if [ -z "$response_size" ]; then
-    echo "Content-Length header not found in the response."
-else
-    echo "Size of the response body: $response_size bytes"
-fi
+# script that takes in a URL, sends a request to that URL,
+# and displays size of the body of the response.
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
